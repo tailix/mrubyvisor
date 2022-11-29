1899ea2b6e5085a8eec7abbf791fffd11529b5d1
Alex Kotov <kotovalexarian@gmail.com>
2022-11-29 23:38:32 +0400

    Request ELF symbols from Multiboot 2 bootloader

--- src/multiboot2.c
+++ src/multiboot2.c
@@ -1,14 +1,23 @@
+#include <stdint.h>
+
 #include <kernaux/multiboot2.h>
 
 #include <kernaux/macro/packing_start.run>
 
 __attribute__((
     section(".multiboot2"),
-    used,
-    aligned(KERNAUX_MULTIBOOT2_HEADER_ALIGN)
+    aligned(KERNAUX_MULTIBOOT2_HEADER_ALIGN),
+    used
 ))
 const struct {
     struct KernAux_Multiboot2_Header header;
+
+    struct {
+        struct KernAux_Multiboot2_HTag_InfoReq tag;
+        uint32_t mbi_tag_types[1];
+    } KERNAUX_PACKED tag_info_req;
+    uint8_t _align1[4];
+
     struct KernAux_Multiboot2_HTag_None tag_none;
 }
 KERNAUX_PACKED
@@ -21,6 +30,18 @@ multiboot2 = {
             KERNAUX_MULTIBOOT2_HEADER_ARCH_I386,
             sizeof(multiboot2)
         ),
+    },
+    .tag_info_req = {
+        .tag = {
+            .base = {
+                .type = KERNAUX_MULTIBOOT2_HTAG_INFO_REQ,
+                .flags = KERNAUX_MULTIBOOT2_HTAG_BASE_FLAG_OPTIONAL,
+                .size = sizeof(multiboot2.tag_info_req),
+            },
+        },
+        .mbi_tag_types = {
+            KERNAUX_MULTIBOOT2_ITAG_ELF_SYMBOLS,
+        },
     },
     .tag_none = {
         .base = {
